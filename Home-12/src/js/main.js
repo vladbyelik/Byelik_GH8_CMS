18d1fb7e67bef81755ee0e30e66155a691708acf
vladbyelik <vladbyelik@gmail.com>
2019-01-06 02:03:07 +0200

    Finished Home12

--- Home-12/src/js/main.js
+++ Home-12/src/js/main.js
@@ -2,7 +2,6 @@ var num1;
 var num2;
 var result;
 
-
 function plus() {
   num1 = document.getElementById('value1').value;
   num2 = document.getElementById('value2').value;
@@ -29,39 +28,40 @@ function divide() {
   num2 = document.getElementById('value2').value;
   result = num1 / num2;
   document.getElementById('result').innerHTML = result;
+  if (num2==0) {
+    document.getElementById('result').innerHTML = 'Mistake!';
+  }
 }
 
 function power() {
   num1 = document.getElementById('value1').value;
-  num2 = document.getElementById('value2').value;
   result = num1 * num1;
   document.getElementById('result').innerHTML = result;
 }
 
 function sqrt() {
   num1 = document.getElementById('value1').value;
-  num2 = document.getElementById('value2').value;
-  result = sqrt(num1);
+  result = Math.sqrt(num1);
   document.getElementById('result').innerHTML = result;
 }
 
 function sin() {
   num1 = document.getElementById('value1').value;
-  num2 = document.getElementById('value2').value;
-  //result = num1 - num2;
+  result = Math.sin(num1);
   document.getElementById('result').innerHTML = result;
 }
 
 function cos() {
   num1 = document.getElementById('value1').value;
-  num2 = document.getElementById('value2').value;
-  //result = num1 - num2;
+  result = Math.cos(num1);
   document.getElementById('result').innerHTML = result;
 }
 
 function factorial() {
   num1 = document.getElementById('value1').value;
-  num2 = document.getElementById('value2').value;
-  //result = num1 - num2;
+  result = 1;
+  while(num1){
+    result *= num1--;
+  }
   document.getElementById('result').innerHTML = result;
 }
